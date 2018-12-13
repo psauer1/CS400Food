1f3aff2bcb4b0456b6e61561130d6de546189040
rley4 <43586126+rley4@users.noreply.github.com>
2018-12-12 19:41:43 -0600

    Final Submission

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,4 +1,22 @@
+/**
+ * Filename:   FoodItem.java
+ * Project:    p5 - Meal Analysis
+ * Authors:    Peter Sauer, Riley Ley, Taylor Chappell, Andy Boho, Nathan Husemoller
+ *
+ * Semester:   Fall 2018
+ * Course:     CS400
+ * Lecture:    002 & 003
+ * 
+ * 
+ * Due Date:   Before 10pm on December 12, 2018
+ * Version:    1.0
+ * 
+ * Credits:    aka
+ *
+ * Bugs:       
+ */
 
+package application;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,18 +28,19 @@ import java.util.List;
  * @author aka
  */
 public class FoodItem {
-	
-	/*fields*/
-	
-	private String name;	// The name of the food item.
-	private String id;	// The id of the food item.
-	private HashMap<String, Double> nutrients = new HashMap<>();	// Map of nutrients and value.
+
+	/* fields */
+	private String name; // The name of the food item.
+	private String id; // The id of the food item.
+	private HashMap<String, Double> nutrients = new HashMap<>(); // Map of nutrients and value.
 
 	/**
 	 * Constructor
 	 * 
-	 * @param name name of the food item
-	 * @param id unique id of the food item
+	 * @param name
+	 *            name of the food item
+	 * @param id
+	 *            unique id of the food item
 	 */
 	public FoodItem(String id, String name) {
 		this.name = name;
@@ -31,17 +50,19 @@ public class FoodItem {
 	/**
 	 * Constructor
 	 * 
-	 * @param name name of the food item
-	 * @param id unique id of the food item
+	 * @param name
+	 *            name of the food item
+	 * @param id
+	 *            unique id of the food item
 	 */
 	public FoodItem(String id, String name, double cal, double fat, double protein, double carb, double fiber) {
 		this.name = name;
 		this.id = id;
-		addNutrient("calories",cal);
-		addNutrient("fat",fat);
-		addNutrient("protein",protein);
-		addNutrient("carb",carb);
-		addNutrient("fiber",fiber);
+		addNutrient("calories", cal);
+		addNutrient("fat", fat);
+		addNutrient("protein", protein);
+		addNutrient("carb", carb);
+		addNutrient("fiber", fiber);
 	}
 
 	/**
@@ -72,13 +93,14 @@ public class FoodItem {
 	}
 
 	/**
-	 * Adds a nutrient and its value to this food.
-	 * If nutrient already exists, updates its value.
+	 * Adds a nutrient and its value to this food. If nutrient already exists,
+	 * updates its value.
 	 * 
-	 * @param name, value
+	 * @param name,
+	 *            value
 	 */
 	public void addNutrient(String name, double value) {
-		nutrients.put(name,value);
+		nutrients.put(name, value);
 	}
 
 	/**
@@ -90,17 +112,17 @@ public class FoodItem {
 	 * @param carb
 	 * @param fiber
 	 */
-	public void addAllNutrients(double cal, double fat, double protein, double carb, double fiber){
-		addNutrient("calories",cal);
-		addNutrient("fat",fat);
-		addNutrient("protein",protein);
-		addNutrient("carb",carb);
-		addNutrient("fiber",fiber);
+	public void addAllNutrients(double cal, double fat, double protein, double carb, double fiber) {
+		addNutrient("calories", cal);
+		addNutrient("fat", fat);
+		addNutrient("protein", protein);
+		addNutrient("carb", carb);
+		addNutrient("fiber", fiber);
 	}
-	
+
 	/**
-	 * Returns the value of the given nutrient value for this food item
-	 * Returns 0 if food item is not found
+	 * Returns the value of the given nutrient value for this food item Returns 0 if
+	 * food item is not found
 	 * 
 	 * @param name
 	 * @return
@@ -109,13 +131,15 @@ public class FoodItem {
 		return nutrients.get(name);
 	}
 
-	/*accessors*/
-	
+	/* accessors */
+
 	public double getCal() {
 		return nutrients.get("calories");
 	}
 
-	public double getFat() { return nutrients.get("fat"); }
+	public double getFat() {
+		return nutrients.get("fat");
+	}
 
 	public double getProtein() {
 		return nutrients.get("protein");
